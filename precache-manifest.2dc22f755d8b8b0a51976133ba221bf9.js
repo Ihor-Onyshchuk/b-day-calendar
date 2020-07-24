084a9d3365296a37f3739eaf307b09df6bf66b89
ihor <onyshchukihor11@gmail.com>
2020-07-24 22:28:34 +0300

    Updates

---
+++ precache-manifest.2dc22f755d8b8b0a51976133ba221bf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcdb572c5abd5573a82a478eeca07c3",
+    "url": "/b-day-calendar/index.html"
+  },
+  {
+    "revision": "ada88995abceea5e8e11",
+    "url": "/b-day-calendar/static/css/main.91ca8116.chunk.css"
+  },
+  {
+    "revision": "7cfb63568f58fa29d0e9",
+    "url": "/b-day-calendar/static/js/2.91e931bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/b-day-calendar/static/js/2.91e931bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada88995abceea5e8e11",
+    "url": "/b-day-calendar/static/js/main.bcdaf7eb.chunk.js"
+  },
+  {
+    "revision": "287d7214dbac0f132ba4",
+    "url": "/b-day-calendar/static/js/runtime-main.c4427cba.js"
+  },
+  {
+    "revision": "bd85ac8c8c40fdad68cfe0101a744c8a",
+    "url": "/b-day-calendar/static/media/load-indicator.bd85ac8c.gif"
+  },
+  {
+    "revision": "16fce02fbfbd0248f2144f47e18d471d",
+    "url": "/b-day-calendar/static/media/user.16fce02f.png"
+  }
+]);
